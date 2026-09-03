45c754179e3a86d8760c594074909bc9b624668e
Mahmoud Tabikh <mahmoud.tabikh@hotmail.com>
2026-09-03 15:57:27 +0200

    changed model

--- src/rag/embeddings.py
+++ src/rag/embeddings.py
@@ -2,12 +2,10 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from src.rag.build_index import load_documents
 
-model = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-L6-v2",
-)
+embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 
 documents = load_documents("/home/mahmoud/projects/RegionAwareVisionAssistant/docs")
 
-vector_store = QdrantVectorStore.from_documents(documents, embedding=model, location=":memory:", collection_name="methodology_docs")
+vector_store = QdrantVectorStore.from_documents(documents, embedding=embeddings, location=":memory:", collection_name="methodology_docs")
 
 vector_store.similarity_search("wood defect threshold")
